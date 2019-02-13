aee59bfd8b7c1c52d89646384987a4e5cb8bfdf8
Samir Musali <samir.musali@gmail.com>
2019-02-13 02:56:56 -0500

    SSOLogin Fix (#25)
    * Updating Callbacks
    
    * Updating Git Ignore
    
    * Touching SaveConfig
    
    * Some Updates
    
    * Some Updates
    
    * It's Done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+#!/usr/bin/env node
+/* globals process */
+// override es6 promise with bluebird
+Promise = require('bluebird'); // eslint-disable-line
 const _ = require('lodash');
 const program = require('commander');
-const properties = require('properties');
+const properties = Promise.promisifyAll(require('properties'));
 const qs = require('querystring');
+const os = require('os');
 
 const pkg = require('./package.json');
 const WebSocket = require('./lib/logdna-websocket');
 const input = require('./lib/input');
 const utils = require('./lib/utils');
 const EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+var config = require('./lib/config');
+var isWinAdmin;
 
 process.title = 'logdna';
 program._name = 'logdna';
@@ -31,12 +39,39 @@ program
         utils.log();
     });
 
-properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
-    path: true
-}, function(error, parsedConfig) {
-    const config = _.merge(require('./lib/config'), parsedConfig || {});
+if (os.platform() === 'win32') isWinAdmin = require('is-administrator');
 
-    utils.performUpgrade(config, function() {
+function checkElevated() {
+    return new Promise(resolve => {
+        if (os.platform() === 'win32') {
+            resolve(isWinAdmin());
+        } else if (process.getuid() <= 0) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+};
+
+checkElevated()
+    .then(isElevated => {
+        if (!isElevated) {
+            console.log('You must be an Administrator (root, sudo) run this agent! See -h or --help for more info.');
+            process.exit();
+        }
+
+        return properties.parseAsync(config.DEFAULT_CONF_FILE, {
+            path: true
+        }).catch(() => {});
+    })
+    .then(parsedConfig => {
+
+        config = _.merge(config, parsedConfig || {});
+
+        utils.performUpgrade(config, error => {
+            if (error) utils.log(error);
+        });
+
         program.command('register [email] [key]')
             .description('Register a new LogDNA account. [key] is optional and will autogenerate')
             .action(function(email, key) {
@@ -58,7 +93,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
                                     , firstname: firstname
                                     , lastname: lastname
                                     , company: company
-                                }, function(body) {
+                                }, function(error, body) {
+                                    if (error) return utils.log(error);
                                     config.email = email;
 
                                     if (config.account !== body.account) {
@@ -73,15 +109,15 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
 
                                     if (body.servicekeys && body.servicekeys.length) config.servicekey = body.servicekeys[0];
 
-                                    utils.saveConfig(config, function() {
+                                    utils.saveConfig(config, function(error, success) {
+                                        if (error) return utils.log(error);
                                         utils.log('Thank you for signing up! Your Ingestion Key is: ' + body.key + '. Saving credentials to local config.');
                                         utils.log();
                                         utils.log('Next steps:');
                                         utils.log('===========');
                                         utils.log('1) We\'ve sent you a welcome email to create your password. Once set, come back here and use \'logdna login\'');
                                         utils.log('2) Type \'logdna install\' for more info on collecting your logs via our agent, syslog, Heroku, API, etc.');
-                                        utils.log();
-                                        return;
+                                        return utils.log();
                                     });
                                 });
                             });
@@ -106,17 +142,15 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
 
                 // Overide SSO URL if using a custom environment
                 var sso_url = config.SSO_URL;
-                if (config.LOGDNA_APPHOST) {
-                    sso_url = config.LOGDNA_APPHOST + config.SSO_LONG_PATH;
-                }
-
+                if (config.LOGDNA_APPHOST) sso_url = config.LOGDNA_APPHOST + config.SSO_LONG_PATH;
                 utils.log('To sign in via SSO, use a web browser to open the page ' + sso_url + token);
 
                 var pollToken = function() {
                     utils.apiPost(config, 'sso', {
                         auth: false
                         , token: token
-                    }, function(body) {
+                    }, function(error, body) {
+                        if (error) return utils.log(error);
                         if (!body || !body.email) return;
                         clearTimeout(pollTimeout);
 
@@ -130,7 +164,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
 
                         if (body.servicekeys && body.servicekeys.length) config.servicekey = body.servicekeys[0];
 
-                        utils.saveConfig(config, function() {
+                        utils.saveConfig(config, function(error, success) {
+                            if (error) return utils.log(error);
                             utils.log('Logged in successfully as: ' + body.email + '. Saving credentials to local config.');
                         });
                     });
@@ -165,7 +200,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
 
                             if (body && body.servicekeys && body.servicekeys.length) config.servicekey = body.servicekeys[0];
 
-                            utils.saveConfig(config, function() {
+                            utils.saveConfig(config, function(error, success) {
+                                if (error) return utils.log(error);
                                 utils.log('Logged in successfully as: ' + email + '. Saving credentials to local config.');
                             });
                         });
@@ -225,9 +261,7 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
                         data.p.forEach(function(line) {
                             utils.renderLine(config, line, params);
                         });
-                    } else {
-                        utils.renderLine(config, data.p, params);
-                    }
+                    } else utils.renderLine(config, data.p, params);
                 });
 
                 ws.on('error', function(err) {
@@ -249,7 +283,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
         program.command('switch')
             .description('Switch between multiple accounts if your login has access to more than one')
             .action(function(options) {
-                utils.apiGet(config, 'orgs', {}, function(body) {
+                utils.apiGet(config, 'orgs', {}, function(error, body) {
+                    if (error) return utils.log(error);
                     body = JSON.parse(body);
                     if (!body || (body.length && body.length < 2)) return utils.log('Your login ' + config.email + ' doesn\'t belong to other accounts. Ensure the other owner has added your email.');
 
@@ -266,7 +301,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
 
                         config.account = body[selection].id;
                         config.servicekey = body[selection].servicekeys[0];
-                        utils.saveConfig(config, function() {
+                        utils.saveConfig(config, function(error, success) {
+                            if (error) return utils.log(error);
                             utils.log('Successfully switched account to ' + body[selection].name);
                         });
                     });
@@ -329,7 +365,7 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
                 var modifiedconfig = JSON.parse(JSON.stringify(config));
 
                 // this prevents export API from emailing the results
-                delete modifiedconfig.email;
+                modifiedconfig = _.omit(modifiedconfig, ['email']);
 
                 var t, t2, range;
 
@@ -378,7 +414,9 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
                     });
 
                     config.last_timestamp = last_timestamp.toJSON();
-                    utils.saveConfig(config);
+                    utils.saveConfig(config, function(error, success) {
+                        if (error) return utils.log(error);
+                    });
                 });
             });
 
@@ -386,7 +424,8 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
             .alias('whoami')
             .description('Show current logged in user info')
             .action(function() {
-                utils.apiGet(config, 'info', function(body) {
+                utils.apiGet(config, 'info', function(error, body) {
+                    if (error) return utils.log(error);
                     utils.log(body);
                 });
             });
@@ -405,4 +444,21 @@ properties.parse(require('./lib/config').DEFAULT_CONF_FILE, {
         program.parse(process.argv);
         if (!process.argv.slice(2).length) return program.outputHelp(); // show help if no commands given
     });
+
+Promise.onPossiblyUnhandledRejection(function(error) {
+    throw error;
 });
+
+process.on('uncaughtException', function(err) {
+    utils.log('------------------------------------------------------------------');
+    utils.log('Uncaught Error: ' + (err.stack || '').split('\r\n'));
+    utils.log('------------------------------------------------------------------');
+});
+
+process.once('SIGTERM', function() {
+    utils.log('Got SIGTERM signal, shutting down...');
+}); // kill
+
+process.once('SIGINT', function() {
+    utils.log('Got SIGINT signal, shutting down...');
+}); // ctrl+c
